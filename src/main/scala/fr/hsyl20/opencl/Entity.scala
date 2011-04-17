796824720109b1323270a5cd1cb177d54d9240af
Sylvain HENRY <hsyl20@yahoo.fr>
2011-04-17 07:03:51 +0200

    Remove debug string

--- src/main/scala/fr/hsyl20/opencl/Entity.scala
+++ src/main/scala/fr/hsyl20/opencl/Entity.scala
@@ -90,7 +90,6 @@ trait Retainable extends Entity {
 
    override protected def finalize: Unit = {
       try {
-        scala.Console.err.println("releasing: "+this)
          release
       } catch {
          case e:OpenCLException => ()

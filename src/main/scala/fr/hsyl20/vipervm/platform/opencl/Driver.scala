dc2e3e05786b6c2516b8e0a6ef070e6be55d5b42
Sylvain HENRY <hsyl20@yahoo.fr>
2011-04-18 02:32:59 +0200

    Add options to OpenCL driver to select device types

--- src/main/scala/fr/hsyl20/vipervm/platform/opencl/Driver.scala
+++ src/main/scala/fr/hsyl20/vipervm/platform/opencl/Driver.scala
@@ -16,13 +16,34 @@ package fr.hsyl20.vipervm.platform.opencl
 import fr.hsyl20.vipervm.platform._
 import fr.hsyl20.{opencl => cl}
 
-/** OpenCL Driver
+/**
+ * OpenCL platform driver
+ *
+ * @param enableGPUs Enable GPU devices
+ * @param enableCPUs Enable CPU devices
+ * @param enableAccelerators Enable accelerator devices
  */
-class OpenCLDriver extends Driver {
+class OpenCLDriver(
+    enableGPUs:Boolean = true,
+    enableCPUs:Boolean = false,
+    enableAccelerators:Boolean = true)
+  extends Driver {
+
+  implicit def richBoolean(b:Boolean) = new {
+    def apply[A](t:A,f:A):A = if (b) t else f
+  }
    
-  val processors:Seq[OpenCLProcessor] = cl.OpenCL.platforms.flatMap(_.devices()).map(new OpenCLProcessor(_))
+  val processors:Seq[OpenCLProcessor] = {
+    import cl.Device._
+    val enabledDeviceTypes = enableGPUs(CL_DEVICE_TYPE_GPU,0) |
+                       enableCPUs(CL_DEVICE_TYPE_CPU,0) |
+                       enableAccelerators(CL_DEVICE_TYPE_ACCELERATOR,0)
+
+    cl.OpenCL.platforms.flatMap(_.devices(enabledDeviceTypes)).map(new OpenCLProcessor(_))
+  }
 
   val networks:Seq[OpenCLNetwork] = processors.map(new OpenCLNetwork(_))
 
   val memories:Seq[OpenCLMemoryNode] = processors.map(new OpenCLMemoryNode(_))
+
 }

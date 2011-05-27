22201b31702bd82f47e83ae4d3aa5052b28ae56f
Sylvain HENRY <sylvain.henry@inria.fr>
2011-05-27 11:47:36 +0200

    Introduce morphisms

---
+++ src/main/scala/org/vipervm/data/morphisms/Homomorphism.scala
@@ -0,0 +1,18 @@
+/*                                                  *\
+** \ \     / _)                   \ \     /   \  |  **
+**  \ \   /   |  __ \    _ \   __| \ \   /   |\/ |  **
+**   \ \ /    |  |   |   __/  |     \ \ /    |   |  **
+**    \_/    _|  .__/  \___| _|      \_/    _|  _|  **
+**              _|                                  **
+**                                                  **
+**       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~          **
+**                                                  **
+**            http://www.vipervm.org                **
+**                     GPLv3                        **
+\*                                                  */
+
+package org.vipervm.data.morphisms
+
+trait Homomorphism extends Morphism {
+  assert(domain == codomain)
+}
---
+++ src/main/scala/org/vipervm/data/morphisms/Morphism.scala
@@ -0,0 +1,24 @@
+/*                                                  *\
+** \ \     / _)                   \ \     /   \  |  **
+**  \ \   /   |  __ \    _ \   __| \ \   /   |\/ |  **
+**   \ \ /    |  |   |   __/  |     \ \ /    |   |  **
+**    \_/    _|  .__/  \___| _|      \_/    _|  _|  **
+**              _|                                  **
+**                                                  **
+**       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~          **
+**                                                  **
+**            http://www.vipervm.org                **
+**                     GPLv3                        **
+\*                                                  */
+
+package org.vipervm.data.morphisms
+
+import org.vipervm.data.Type
+
+/**
+ * A morphism between two categories
+ */
+trait Morphism {
+  val domain:Type
+  val codomain:Type
+}

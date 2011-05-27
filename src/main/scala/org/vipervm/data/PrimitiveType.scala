f9ae6fad78902d3ae0e304cf6835605b13cb048c
Sylvain HENRY <sylvain.henry@inria.fr>
2011-05-27 11:23:26 +0200

    Add primitive structures

--- src/main/scala/org/vipervm/data/PrimitiveType.scala
+++ src/main/scala/org/vipervm/data/PrimitiveType.scala
@@ -14,8 +14,10 @@
 package org.vipervm.data
 
 sealed abstract class PrimitiveType
-class TDouble extends PrimitiveType
-class TSingle extends PrimitiveType
-class TInt32  extends PrimitiveType
-class TInt64  extends PrimitiveType
-class TBoolean  extends PrimitiveType
+case object DoublePrimitive extends PrimitiveType
+case object FloatPrimitive  extends PrimitiveType
+case object Int32Primitive  extends PrimitiveType
+case object Int64Primitive  extends PrimitiveType
+case object Int16Primitive  extends PrimitiveType
+case object BytePrimitive   extends PrimitiveType
+case class  StructurePrimitive(fields:Seq[PrimitiveType]) extends PrimitiveType

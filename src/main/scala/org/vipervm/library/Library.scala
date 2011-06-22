416bb9148b097e2d49178efa674f7d00669b0d4c
Sylvain HENRY <sylvain.henry@inria.fr>
2011-06-22 15:12:54 +0200

    Introduce kernel library

---
+++ src/main/scala/org/vipervm/library/Library.scala
@@ -0,0 +1,26 @@
+/*                                                  *\
+** \ \     / _)                   \ \     /   \  |  **
+**  \ \   /   |  __ \    _ \   __| \ \   /   |\/ |  **
+**   \ \ /    |  |   |   __/  |     \ \ /    |   |  **
+**    \_/    _|  .__/  \___| _|      \_/    _|  _|  **
+**              _|                                  **
+**                                                  **
+**       ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~          **
+**                                                  **
+**            http://www.vipervm.org                **
+**                     GPLv3                        **
+\*                                                  */
+
+package org.vipervm.library
+
+import org.vipervm.runtime.FunctionalKernel
+
+/**
+ * Kernel library
+ */
+class Library {
+  /** Try to retrieve from its name */
+  def retrieve(name:String): Option[FunctionalKernel] = {
+    throw new Exception("Not implemented")
+  }
+}

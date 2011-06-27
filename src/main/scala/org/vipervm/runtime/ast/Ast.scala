ddfc98fc8aafb34ae1805a1e3d26671e778cad63
Sylvain HENRY <sylvain.henry@inria.fr>
2011-06-27 08:27:49 +0200

    Add shift & subst

--- src/main/scala/org/vipervm/runtime/ast/Ast.scala
+++ src/main/scala/org/vipervm/runtime/ast/Ast.scala
@@ -18,6 +18,30 @@ import org.vipervm.runtime.FunctionalKernel
 
 sealed abstract class Term {
   def apply(ts:Term*) = (this /: ts.toList)(AppTerm(_,_))
+
+  private def map(f:(VarTerm,Int)=>Term,c:Int):Term = this match {
+    case t@VarTerm(_,_) => f(t,c)
+    case AbsTerm(body,name) => AbsTerm(body.map(f,c+1),name)
+    case AppTerm(left,right) => AppTerm(left.map(f,c),right.map(f,c))
+  }
+
+  def shift(d:Int):Term = {
+    val f = (v:VarTerm,c:Int) => {
+      if (v.idx >= c) VarTerm(v.idx+d,v.ctxSize+d) else VarTerm(v.idx, v.ctxSize+d)
+    }
+    this.map(f,0)
+  }
+
+  def subst(j:Int,s:Term):Term = {
+    val f = (v:VarTerm,c:Int) => {
+      if (v.idx == j+c) s.shift(c) else VarTerm(v.idx,v.ctxSize)
+    }
+    this.map(f,0)
+  }
+
+  def substTop(s:Term):Term = {
+    subst(0,s.shift(1)).shift(-1)
+  }
 }
 
 /**

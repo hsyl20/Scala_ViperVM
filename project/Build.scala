8ccaaf0abf7a19db9d4fa1bc7401d7a300800e41
Sylvain HENRY <sylvain.henry@inria.fr>
2011-11-26 16:39:00 +0100

    Cleaned SBT config file

--- project/Build.scala
+++ project/Build.scala
@@ -1,19 +1,60 @@
 import sbt._
 import Keys._
 
-object BuildSettings {
+object MyProject extends Build {
   val buildOrganization = "org.vipervm"
   val buildName         = "ViperVM"
   val buildVersion      = "0.1"
   val buildScalaVersion = "2.9.1"
 
-  val buildSettings = Defaults.defaultSettings ++ Seq (
+  lazy val project = Project (buildName, file("."), settings = mySettings)
+
+  val mySettings = Defaults.defaultSettings ++ Seq (
     organization := buildOrganization,
     name         := buildName,
     version      := buildVersion,
     scalaVersion := buildScalaVersion,
-    shellPrompt  := ShellPrompt.buildShellPrompt
+    scalacOptions := Seq("-deprecation", "-unchecked"),
+    resolvers    := myResolvers,
+    libraryDependencies := myDependencies,
+
+    /* Do not interfer with OpenCL signals */
+    fork := true,
+    javaOptions += "-Xrs",
+
+    /* GIT ready shell prompt */
+    shellPrompt  := ShellPrompt.buildShellPrompt,
+
+    /* Tasks */
+    fullRunTask(TaskKey[Unit]("run-demo-codegen", "C Code generation demo"), Test, "demos.codegen.Main"),
+    fullRunTask(TaskKey[Unit]("run-profiler", "Profiler GUI"), Test, "org.vipervm.apps.Profiler"),
+    fullRunTask(TaskKey[Unit]("run-platform-info", "Platform information"), Test, "org.vipervm.apps.Info")
   )
+
+  val myResolvers = Seq(
+    "Sun Maven2 Repo" at "http://download.java.net/maven/2",
+    "Oracle Maven2 Repo" at "http://download.oracle.com/maven",
+    "Scala-Tools Snapshots" at "http://scala-tools.org/repo-snapshots"
+  )
+
+  val myDependencies = Seq (
+    "org.scala-lang" % "jline" % buildScalaVersion,
+    "org.scala-lang" % "scala-swing" % buildScalaVersion,
+    "net.java.dev.jna" % "jna" % "3.3.0",
+    "commons-io" % "commons-io" % "1.4",
+    "org.clapper" %% "grizzled-slf4j" % "0.6.6",
+    "org.scalaz" %% "scalaz-full" % "6.0.3",
+    "org.scalatest" %% "scalatest" % "1.6.1",
+    /* Logging configuration :
+     * - Simple: Output logs to System.err
+     * - NOP: Discard logging 
+     * - log4j 
+     */
+    "org.slf4j" % "slf4j-simple" % "1.6.2"
+  //"org.slf4j" % "slf4j-nop" % "1.6.2"
+  //"org.slf4j" % "slf4j-log4j12" % "1.6.2"
+  )
+
 }
 
 // Shell prompt which show the current project, 
@@ -35,104 +76,8 @@ object ShellPrompt {
         current findFirstMatchIn gitBranches map (_ group(1)) getOrElse "-"
       val currProject = Project.extract (state).currentProject.id
       "%s:%s:%s> ".format (
-        currProject, currBranch, BuildSettings.buildVersion
+        currProject, currBranch, MyProject.buildVersion
       )
     }
   }
 }
-
-object Resolvers {
-  val sunrepo    = "Sun Maven2 Repo" at "http://download.java.net/maven/2"
-  val oraclerepo = "Oracle Maven2 Repo" at "http://download.oracle.com/maven"
-
-  val scala_tools_snapshots = "Scala-Tools Snapshots" at "http://scala-tools.org/repo-snapshots"
-
-  val  bridj = "nativelibs4java" at "http://nativelibs4java.sourceforge.net/maven/nativelibs4java"
-
-  val allResolvers = Seq(sunrepo, oraclerepo, scala_tools_snapshots)
-}
-
-object Dependencies {
-  import BuildSettings._
-
-  /* JLine for the REPL */
-  val jline = "org.scala-lang" % "jline" % buildScalaVersion
-
-  /* Runtime libs */
-  val jna = "net.java.dev.jna" % "jna" % "3.3.0"
-
-  val apacheCommons = "commons-io" % "commons-io" % "1.4"
-
-  /* GUI libs */
-  val scalaSwing = "org.scala-lang" % "scala-swing" % buildScalaVersion
-
-  /* Logging (SLF4J through grizzled) */
-  val grizzled = "org.clapper" %% "grizzled-slf4j" % "0.6.6"
-
-  /* Logging configuration :
-   * - Simple: Output logs to System.err
-   * - NOP: Discard logging 
-   * - log4j 
-   */
-  val slf4jsimple = "org.slf4j" % "slf4j-simple" % "1.6.2"
-  //val slf4jnop = "org.slf4j" % "slf4j-nop" % "1.6.2"
-  //val slf4jlog4j = "org.slf4j" % "slf4j-log4j12" % "1.6.2"
-
-  /* Others */
-  val scalaz = "org.scalaz" %% "scalaz-full" % "6.0.3"
-
-  val bridj = "com.nativelibs4java" % "bridj" % "0.5"
-
-  /* Test libs */
-  val scalatest = "org.scalatest" %% "scalatest" % "1.6.1"
-  val junit = "junit" % "junit" % "4.8.2"
-}
-
-
-object Tasks {
-  val demoCodegenTask = TaskKey[Unit]("run-demo-codegen", "C Code generation demo")
-  val profilerTask = TaskKey[Unit]("run-profiler", "Profiler GUI")
-  val infoTask = TaskKey[Unit]("run-platform-info", "Platform information")
-
-  val all = Seq(
-    fullRunTask(demoCodegenTask, Test, "demos.codegen.Main"),
-    fullRunTask(profilerTask, Test, "org.vipervm.apps.Profiler"),
-    fullRunTask(infoTask, Test, "org.vipervm.apps.Info")
-  )
-
-}
-
-object ViperVMBuild extends Build {
-  import Resolvers._
-  import Dependencies._
-  import BuildSettings._
-
-  fork in Tasks.profilerTask := true
-
-  fork in run := true
-  javaOptions in (run) += "-Xrs"
-
-  // Sub-project specific dependencies
-  val commonDeps = Seq (
-    jline,
-    jna,
-    apacheCommons,
-    scalaSwing,
-    grizzled,
-    slf4jsimple,
-    scalaz,
-    scalatest,
-    junit
-  )
-
-  lazy val vipervm = Project (
-    "vipervm",
-    file ("."),
-    settings = buildSettings ++ Seq (scalacOptions := Seq("-deprecation", "-unchecked"),
-                                     resolvers := allResolvers,
-                                     libraryDependencies := commonDeps)
-                             ++ Tasks.all
-  )
-
-}
-

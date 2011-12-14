25416807e83419390fafe6c707128de1cdd8f958
Sylvain HENRY <sylvain.henry@inria.fr>
2011-12-14 19:16:11 +0100

    Set version to snapshot

--- project/Build.scala
+++ project/Build.scala
@@ -4,7 +4,7 @@ import Keys._
 object MyProject extends Build {
   val buildOrganization = "org.vipervm"
   val buildName         = "ViperVM"
-  val buildVersion      = "0.1"
+  val buildVersion      = "0.1-SNAPSHOT"
   val buildScalaVersion = "2.9.1"
 
   lazy val project = Project (buildName, file("."), settings = mySettings)

b6191e34288d0a81dc2453cb28206aad1276ec26
Sylvain HENRY <sylvain.henry@inria.fr>
2011-12-21 16:47:18 +0100

    Add support for untyped lambda calculus

---
+++ src/main/scala/org/vipervm/fp/Lambda.scala
@@ -0,0 +1,59 @@
+package org.vipervm.fp
+
+/** Lambda calculus */
+sealed abstract class LambdaTerm
+
+case class TmVar(info:Info,n:Int,totalLength:Int) extends LambdaTerm
+case class TmAbs(info:Info,name:String,term:LambdaTerm) extends LambdaTerm
+case class TmApp(info:Info,t1:LambdaTerm,t2:LambdaTerm) extends LambdaTerm
+
+object LambdaTerm {
+  def tmMap(onvar:(Int,TmVar)=>TmVar,t:LambdaTerm,c:Int=0):LambdaTerm = t match {
+    case v@TmVar(_,_,_) => onvar(c,v)
+    case TmAbs(fi,x,t1) => TmAbs(fi,x, tmMap(onvar,t1,c+1))
+    case TmApp(fi,t1,t2) => TmApp(fi, tmMap(onvar,t1,c), tmMap(onvar,t2,c))
+  }
+
+  def shift(t:LambdaTerm,step:Int,c:Int=0):LambdaTerm = t match {
+    case TmVar(fi,x,n) => if (x>c) TmVar(fi,x+step,n+step) else TmVar(fi,x,n+step)
+    case TmAbs(fi,x,t1) => TmAbs(fi,x, shift(t1,step,c+1))
+    case TmApp(fi,t1,t2) => TmApp(fi, shift(t1,step,c), shift(t2,step,c))
+  }
+
+  def subst(j:Int,s:LambdaTerm,t:LambdaTerm,c:Int=0):LambdaTerm = t match {
+    case TmVar(fi,x,n) => if (x==j+c) shift(s,c) else TmVar(fi,x,n)
+    case TmAbs(fi,x,t1) => TmAbs(fi,x, subst(j,s,t1,c+1))
+    case TmApp(fi,t1,t2) => TmApp(fi, subst(j,s,t1,c), subst(j,s,t2,c))
+  }
+}
+
+
+class Binding
+
+class Context(items:Vector[(String,Binding)])  {
+  def pickFreshName(x:String):(Context,String) = {
+    if (!items.map(_._1).contains(x)) {
+      (new Context(items :+ (x,new Binding)), x)
+    }
+    else pickFreshName(x+"_")
+  }
+
+  def length = items.length
+
+  def apply(fi:Info,x:Int):String = items(x)._1
+}
+
+object LambdaPrinter {
+
+  def print(ctx:Context,t:LambdaTerm):String = t match {
+    case TmAbs(fi,x,t1) => {
+      val (ctx2,x2) = ctx.pickFreshName(x)
+      "(lambda %s. %s )".format(x2, print(ctx2,t1))
+    }
+    case TmApp(fi,t1,t2) => "(%s %s)".format(print(ctx,t1),print(ctx,t2))
+    case TmVar(fi,x,n) => {
+      if (ctx.length != n) throw new Exception("Bad index")
+      ctx(fi,x)
+    }
+  }
+}

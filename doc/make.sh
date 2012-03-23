4a22a9fa433323d5dde408fb87873cc2c83fe7a0
Sylvain HENRY <hsyl20@gmail.com>
2012-03-23 15:13:59 +0100

    Doc: use a new makefile

--- doc/make.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-tex=ViperVM
-inputs=$TEXINPUTS:src/:build/:src/format
-bib=src
-
-mkdir -p build
-BIBINPUTS=$bib bibtex build/$tex.aux
-TEXINPUTS=$inputs pdflatex -output-directory build src/$tex.tex
